5deaefbeeffbd87388cd782e9ed58c2ef9657b84
N1ghtm <106837073+dn1ghtm@users.noreply.github.com>
2024-10-13 23:33:41 +0200

    Update Discord.py

--- TwitchChannelPointsMiner/classes/Discord.py
+++ TwitchChannelPointsMiner/classes/Discord.py
@@ -18,7 +18,7 @@ class Discord(object):
                 url=self.webhook_api,
                 data={
                     "content": dedent(message),
-                    "username": "Twitch Channel Points Miner",
+                    "username": "A-Minor :D",
                     "avatar_url": "https://i.imgur.com/X9fEkhT.png",
                 },
             )
